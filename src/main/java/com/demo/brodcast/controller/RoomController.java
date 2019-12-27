db9a1d8bf1cc77349cb3e4fdf051d867c37ecc91
이형원 <hwlee@inforex.co.kr>
2019-12-27 15:03:47 +0900

    방송 데이터 생성진행

--- src/main/java/com/demo/brodcast/controller/RoomController.java
+++ src/main/java/com/demo/brodcast/controller/RoomController.java
@@ -1,7 +1,39 @@
 package com.demo.brodcast.controller;
 
-import org.springframework.stereotype.Controller;
+import com.demo.common.code.Status;
+import com.demo.common.vo.JsonOutputVo;
+import com.demo.util.MessageUtil;
+import com.google.gson.Gson;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.RestController;
 
-@Controller
+import java.util.HashMap;
+
+@RestController
 public class RoomController {
+
+    @Autowired
+    MessageUtil messageUtil;
+
+    /**
+     * 방송생성
+     */
+    @PostMapping("brod")
+    public String brod( @RequestParam(value = "s_gubun", required = true, defaultValue = "BROD00100") String s_gubun,
+                        @RequestParam(value = "s_bgPath", required = false) String s_bgPath,
+                        @RequestParam(value = "s_bgNm", required = false) String s_bgNm,
+                        @RequestParam(value = "s_title", required = true, defaultValue = "제목") String s_title,
+                        @RequestParam(value = "s_intro", required = true, defaultValue = "안녕하세요!") String s_intro,
+                        @RequestParam(value = "s_fan", required = true, defaultValue = "N") String s_fan,
+                        @RequestParam(value = "s_over20", required = true, defaultValue = "N") String s_over20){
+
+        HashMap map = new HashMap();
+        map.put("result", "success");
+        map.put("brodNo", "BRD00121");
+
+        return new Gson().toJson(messageUtil.setJsonOutputVo(new JsonOutputVo(Status.조회, map)));
+    }
 }
