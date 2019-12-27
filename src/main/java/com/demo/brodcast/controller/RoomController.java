f8617373b48b1ff1f95e737a3f03547d8790eb11
이재호 <leejaeho114@gmail.com>
2019-12-27 16:45:43 +0900

    방송 rest api 개발중

--- src/main/java/com/demo/brodcast/controller/RoomController.java
+++ src/main/java/com/demo/brodcast/controller/RoomController.java
@@ -5,14 +5,12 @@ import com.demo.common.vo.JsonOutputVo;
 import com.demo.util.MessageUtil;
 import com.google.gson.Gson;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestParam;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
 import java.util.HashMap;
 
 @RestController
+@RequestMapping("/brod")
 public class RoomController {
 
     @Autowired
@@ -21,7 +19,7 @@ public class RoomController {
     /**
      * 방송생성
      */
-    @PostMapping("brod")
+    @PostMapping("")
     public String brod( @RequestParam(value = "s_gubun", required = true, defaultValue = "BROD00100") String s_gubun,
                         @RequestParam(value = "s_bgPath", required = false) String s_bgPath,
                         @RequestParam(value = "s_bgNm", required = false) String s_bgNm,
