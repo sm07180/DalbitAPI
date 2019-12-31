b036791a891efff072d902ece7d1f601d7087df8
이재호 <leejaeho114@gmail.com>
2019-12-31 10:34:11 +0900

    Redis 설정 변경

--- src/main/java/com/demo/config/RedisRepositoryConfig.java
+++ src/main/java/com/demo/config/RedisRepositoryConfig.java
@@ -4,6 +4,7 @@ import org.springframework.beans.factory.annotation.Value;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.data.redis.connection.RedisConnectionFactory;
+import org.springframework.data.redis.connection.RedisStandaloneConfiguration;
 import org.springframework.data.redis.connection.lettuce.LettuceConnectionFactory;
 import org.springframework.data.redis.core.RedisTemplate;
 import org.springframework.data.redis.repository.configuration.EnableRedisRepositories;
@@ -22,15 +23,20 @@ public class RedisRepositoryConfig {
     @Value("${spring.redis.port}")
     private int redisPort;
 
+    @Value("${spring.redis.password}")
+    private String redisPassword;
+
     @Value("${spring.redis.database}")
     private int redisDatabase;
 
     @Bean
     public RedisConnectionFactory redisConnectionFactory() {
 
-        LettuceConnectionFactory lettuceConnectionFactory = new LettuceConnectionFactory(redisHost, redisPort);
-        lettuceConnectionFactory.setDatabase(redisDatabase);
+        RedisStandaloneConfiguration redisConfiguration = new RedisStandaloneConfiguration(redisHost, redisPort);
+        redisConfiguration.setPassword(redisPassword);
+        redisConfiguration.setDatabase(redisDatabase);
 
+        LettuceConnectionFactory lettuceConnectionFactory = new LettuceConnectionFactory(redisConfiguration);
         return lettuceConnectionFactory;
     }
 
