096323abc1b7bab44c57e2dcba7392bcf268ecc4
이재호 <leejaeho114@gmail.com>
2019-12-31 10:34:48 +0900

    global exception 수정

--- src/main/java/com/demo/exception/GlobalException.java
+++ src/main/java/com/demo/exception/GlobalException.java
@@ -2,14 +2,28 @@ package com.demo.exception;
 
 import com.demo.common.code.ErrorStatus;
 import com.demo.exception.vo.ExceptionVo;
+import lombok.Getter;
+import lombok.Setter;
 import lombok.ToString;
 
 import java.util.HashMap;
 
 @ToString
+@Setter
+@Getter
 public class GlobalException extends Exception {
 
     private static final long serialVersionUID = 1L;
+
+    private String message;
+    private String errorCode;
+    private String messageKey;
+    private ErrorStatus errorStatus;
+
+    GlobalException(){}
+    GlobalException(ErrorStatus errorStatus){
+        setErrorStatus(errorStatus);
+    }
 
     public static ExceptionVo throwException(ErrorStatus errorStatus){
         return throwException(errorStatus, null);
