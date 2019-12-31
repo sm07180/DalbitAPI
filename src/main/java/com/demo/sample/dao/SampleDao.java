c20fecfedfb9a223ebc8705dfa02e92aa2baf3ca
이재호 <leejaeho114@gmail.com>
2019-12-31 14:06:28 +0900

    dao interface로 변경

--- src/main/java/com/demo/sample/dao/SampleDao.java
+++ src/main/java/com/demo/sample/dao/SampleDao.java
@@ -2,37 +2,16 @@ package com.demo.sample.dao;
 
 import com.demo.common.vo.ProcedureVo;
 import com.demo.common.vo.SampleVo;
-import org.mybatis.spring.SqlSessionTemplate;
-import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Repository;
 
 import java.util.List;
 
 @Repository
-public class SampleDao {
-    @Autowired
-    private SqlSessionTemplate sqlSession;
+public interface SampleDao {
 
-    private String statementPrefix = "sample.";
-
-    public int getCount(){
-        return this.sqlSession.selectOne(statementPrefix + "getCount");
-    }
-
-    public List<SampleVo> getList(){
-        return this.sqlSession.selectList(statementPrefix + "getList");
-    }
-
-    public int insertSample(SampleVo sampleVo){
-        return this.sqlSession.insert(statementPrefix + "insertSample", sampleVo);
-    }
-
-    public ProcedureVo getNickNameCheck(ProcedureVo procedureVo) {
-        return this.sqlSession.selectOne(statementPrefix + "getNickNameCheck", procedureVo);
-    }
-
-    public List<SampleVo> selectLogData() {
-        return this.sqlSession.selectList(statementPrefix + "selectLogData");
-    }
-
+    int getCount();
+    List<SampleVo> getList();
+    int insertSample(SampleVo sampleVo);
+    ProcedureVo getNickNameCheck(ProcedureVo procedureVo);
+    List<SampleVo> selectLogData();
 }
