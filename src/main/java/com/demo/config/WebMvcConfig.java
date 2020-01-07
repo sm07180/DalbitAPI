111e9e7cef2421512e6f246831ad6f18a1a4d73a
이재은 <coreano81@inforex.co.kr>
2020-01-07 15:11:43 +0900

    파람 이름으로 값 체크

---
+++ src/main/java/com/demo/config/WebMvcConfig.java
@@ -0,0 +1,17 @@
+package com.demo.config;
+
+import org.springframework.context.annotation.Configuration;
+import org.springframework.web.servlet.config.annotation.InterceptorRegistry;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurerAdapter;
+
+import com.demo.interceptor.ParamCheckInterceptor;
+
+@Configuration
+public class WebMvcConfig extends WebMvcConfigurerAdapter {
+
+    @Override
+    public void addInterceptors(InterceptorRegistry registry) {
+        registry.addInterceptor(new ParamCheckInterceptor())
+                .addPathPatterns("/*");
+    }
+}
