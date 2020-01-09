5ed511bda585d88c246a0de94b0552c238450b92
이재은 <coreano81@inforex.co.kr>
2020-01-09 12:53:02 +0900

    Cors 처리

--- src/main/java/com/demo/config/WebMvcConfig.java
+++ src/main/java/com/demo/config/WebMvcConfig.java
@@ -1,7 +1,9 @@
 package com.demo.config;
 
 import com.demo.interceptor.ParamCheckInterceptor;
+import io.swagger.models.HttpMethod;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.web.servlet.config.annotation.CorsRegistry;
 import org.springframework.web.servlet.config.annotation.InterceptorRegistry;
 import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 
@@ -12,5 +14,14 @@ public class WebMvcConfig implements WebMvcConfigurer {
     public void addInterceptors(InterceptorRegistry registry) {
         registry.addInterceptor(new ParamCheckInterceptor())
                 .addPathPatterns("/*");
+    }
+
+    @Override
+    public void addCorsMappings(CorsRegistry registry){
+        registry.addMapping("/*")
+                .allowedOrigins("*")
+                .allowedMethods(HttpMethod.POST.name())
+                .allowCredentials(false)
+                .maxAge(360);
     }
 }
