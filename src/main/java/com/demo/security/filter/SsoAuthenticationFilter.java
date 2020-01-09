948da27e849cf037755bdd35d7d5247f67134647
이형원 <hwlee@inforex.co.kr>
2020-01-09 11:33:42 +0900

    [임시] CORS 헤더 Methods OPTIONS 추가 테스트

--- src/main/java/com/demo/security/filter/SsoAuthenticationFilter.java
+++ src/main/java/com/demo/security/filter/SsoAuthenticationFilter.java
@@ -56,7 +56,7 @@ public class SsoAuthenticationFilter implements Filter {
     public void doFilter(ServletRequest servletRequest, ServletResponse servletResponse, FilterChain filterChain) throws IOException, ServletException {
         HttpServletRequest request = (HttpServletRequest) servletRequest;
         HttpServletResponse response = (HttpServletResponse)servletResponse;
-        response.setHeader("Access-Control-Allow-Methods", "POST, GET, DELETE");
+        response.setHeader("Access-Control-Allow-Methods", "POST, GET, OPTIONS, DELETE");
         response.setHeader("Access-Control-Max-Age", "3600");
         response.setHeader("Access-Control-Allow-Headers", "x-requested-with");
         response.setHeader("Access-Control-Allow-Origin", "*");
