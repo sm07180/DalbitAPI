22b1d606668a06f153c6197e8304565a66f0dd04
이재은 <coreano81@inforex.co.kr>
2020-01-10 09:34:19 +0900

    Cors 처리

--- src/main/java/com/demo/security/filter/SsoAuthenticationFilter.java
+++ src/main/java/com/demo/security/filter/SsoAuthenticationFilter.java
@@ -56,6 +56,10 @@ public class SsoAuthenticationFilter implements Filter {
     public void doFilter(ServletRequest servletRequest, ServletResponse servletResponse, FilterChain filterChain) throws IOException, ServletException {
         HttpServletRequest request = (HttpServletRequest) servletRequest;
         HttpServletResponse response = (HttpServletResponse)servletResponse;
+        response.setHeader("Access-Control-Allow-Methods", "POST, GET, DELETE, OPTIONS");
+        response.setHeader("Access-Control-Max-Age", "3600");
+        response.setHeader("Access-Control-Allow-Headers", "Origin,Accept,X-Requested-With,Content-Type,Access-Control-Request-Method,Access-Control-Request-Headers,Authorization");
+        response.setHeader("Access-Control-Allow-Origin", "*");
 
         if(!isIgnore(request)) {
             Authentication authentication = SecurityContextHolder.getContext().getAuthentication();
