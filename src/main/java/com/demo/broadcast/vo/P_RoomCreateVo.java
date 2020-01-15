2cf6341c7f0fb95a201ac774a2e48a42f9723e7d
이형원 <hwlee@inforex.co.kr>
2020-01-15 10:08:31 +0900

    방송 로직 수정

--- src/main/java/com/demo/broadcast/vo/P_RoomCreateVo.java
+++ src/main/java/com/demo/broadcast/vo/P_RoomCreateVo.java
@@ -9,19 +9,22 @@ import lombok.Getter;
 @AllArgsConstructor
 public class P_RoomCreateVo {
 
-    @Builder.Default private String mem_no = "11577690655946";                                      //회원번호
-    @Builder.Default private String subjectType = "1";                                              //방송종류
-    @Builder.Default private String title = "자유";                                                 //방송제목
-    @Builder.Default private String backgroundImage = "http://backgroundImage.backgroundImage";     //배경이미지 경로
-    @Builder.Default private String backgroundImageGrade = "3";                                     //배경이미지 선전성 등급
-    @Builder.Default private String welcomMsg = "환영합니다";                                       //환영메시지
-    @Builder.Default private String notice = "공지사항";                                            //공지사항
-    @Builder.Default private String entry = "0";                                                    //입장제한
-    @Builder.Default private String age = "0";                                                      //나이제한
-    @Builder.Default private String os = "1";                                                       //OS구분
-    @Builder.Default private String bj_streamid = "bj_streamid00001";                               //bj스트림아이디
-    @Builder.Default private String bj_publish_tokenid = "bj_publish_tokenid00001";                 //bj토큰아이디
-    @Builder.Default private String bj_play_tokenid = "bj_play_tokenid00001";                       //play토큰
+    @Builder.Default private String mem_no = "11577690655946";                                                  //회원번호
+    @Builder.Default private int subjectType = 1;                                                               //방송종류
+    @Builder.Default private String title = "테스트000001";                                                     //방송제목
+    @Builder.Default private String backgroundImage = "http://backgroundImage.moonlight.co.kr/back00001.jpg";   //배경이미지 경로
+    @Builder.Default private int backgroundImageGrade = 3;                                                      //배경이미지 선전성 등급
+    @Builder.Default private String welcomMsg = "환영합니다~00001.";                                            //환영메시지
+    @Builder.Default private String notice = "공지사항00001";                                                   //공지사항
+    @Builder.Default private int entry = 0;                                                                     //입장제한
+    @Builder.Default private int age = 0;                                                                       //나이제한
+    @Builder.Default private int os = 1;                                                                        //OS구분
+    @Builder.Default private String guest_streamid = "";                                                        //guest 스트림아이디
+    @Builder.Default private String guest_publish_tokenid = "";                                                 //guest 토큰아이디
+    @Builder.Default private String guest_play_tokenid = "";                                                    //guest play토큰
+    @Builder.Default private String bj_streamid = "bj_streamid00001";                                           //bj스트림아이디
+    @Builder.Default private String bj_publish_tokenid = "bj_publish_tokenid00001";                             //bj토큰아이디
+    @Builder.Default private String bj_play_tokenid = "bj_play_tokenid00001";                                   //play토큰
 
     @Builder.Default private String deviceUuid = "2200DDD1-77A";                                    //디바이스 고유아이디
     @Builder.Default private String deviceToken = "45E3156FDE20E7F11AF";                            //디바이스 토큰
