872d06a4fd3f0c4b379756bfc720c5a43fdca45f
이형원 <hwlee@inforex.co.kr>
2020-01-15 15:51:54 +0900

    프로필 조회 @PathVariable -> HttpServletRequest 변경

--- src/main/java/com/demo/member/controller/MemberController.java
+++ src/main/java/com/demo/member/controller/MemberController.java
@@ -10,6 +10,7 @@ import com.demo.member.vo.P_InfoVo;
 import com.demo.member.vo.P_LoginVo;
 import com.demo.util.GsonUtil;
 import com.demo.util.MessageUtil;
+import com.demo.util.StringUtil;
 import com.google.gson.Gson;
 import io.swagger.annotations.ApiOperation;
 import lombok.extern.slf4j.Slf4j;
@@ -17,6 +18,7 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.*;
 
+import javax.servlet.http.HttpServletRequest;
 import java.util.HashMap;
 
 @Slf4j
@@ -51,12 +53,12 @@ public class MemberController {
      * 회원정보 조회
      */
     @ApiOperation(value = "회원정보 조회")
-    @GetMapping("/profile/{memNo}")
-    public String infoView(@PathVariable String memNo) throws GlobalException{
+    @GetMapping("/profile")
+    public String infoView(HttpServletRequest request) throws GlobalException{
 
         P_InfoVo apiData = P_InfoVo.builder()
                 .mem_no(MemberVo.getUserInfo().getMemNo())
-                .target_mem_no(memNo)
+                .target_mem_no(StringUtil.convertRequestParamToString(request, "s_mem_no"))
                 .build();
 
         String result = memberService.callMemberInfoView(apiData);
