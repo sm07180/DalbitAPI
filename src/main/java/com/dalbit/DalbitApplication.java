7357e5b136419dc726c86b01f8c8d7e543df2446
이재호 <leejaeho114@gmail.com>
2020-01-16 16:53:40 +0900

    DemoApplication.java -> DalbitApplication.java 이름 변경

--- src/main/java/com/dalbit/DalbitApplication.java
+++ src/main/java/com/dalbit/DalbitApplication.java
@@ -6,16 +6,16 @@ import org.springframework.boot.builder.SpringApplicationBuilder;
 import org.springframework.boot.web.servlet.support.SpringBootServletInitializer;
 
 @SpringBootApplication
-public class DemoApplication extends SpringBootServletInitializer {
+public class DalbitApplication extends SpringBootServletInitializer {
 
     public static void main(String[] args) {
-        SpringApplication.run(DemoApplication.class, args);
+        SpringApplication.run(DalbitApplication.class, args);
         //String test = "id=1234567890&asd=";
         //System.out.println(test.substring(3, test.indexOf("&")));
     }
 
     @Override
     protected SpringApplicationBuilder configure(SpringApplicationBuilder builder) {
-        return builder.sources(DemoApplication.class);
+        return builder.sources(DalbitApplication.class);
     }
 }
