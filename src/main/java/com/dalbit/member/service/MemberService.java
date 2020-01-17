dbbf4c4861470ca1618f4137b9289d326a404533
이재호 <leejaeho114@gmail.com>
2020-01-17 09:44:08 +0900

    잘못올렸네용.. 수정합니다.

--- src/main/java/com/dalbit/member/service/MemberService.java
+++ src/main/java/com/dalbit/member/service/MemberService.java
@@ -107,66 +107,6 @@ public class MemberService {
     }
 
     /**
-     * 프로필 편집
-     */
-    public String callProfileEdit(P_ProfileEditVo pProfileEditVo){
-        ProcedureVo procedureVo = new ProcedureVo(pProfileEditVo);
-        memberDao.callProfileEdit(procedureVo);
-        String result;
-        if(procedureVo.getRet().equals(Status.프로필편집성공.getMessageCode())) {
-            result = gsonUtil.toJson(messageUtil.setJsonOutputVo(new JsonOutputVo(Status.프로필편집성공, procedureVo.getData())));
-        } else if(procedureVo.getRet().equals(Status.프로필편집실패_닉네임중복.getMessageCode())) {
-            result = gsonUtil.toJson(messageUtil.setJsonOutputVo(new JsonOutputVo(Status.프로필편집실패_닉네임중복)));
-        } else{
-            result = gsonUtil.toJson(messageUtil.setJsonOutputVo(new JsonOutputVo(Status.프로필편집실패_회원아님)));
-        }
-        return result;
-    }
-
-    /**
-     * 팬가입
-     */
-    public String callFanstarInsert(P_FanstarInsertVo pFanstarInsertVo) {
-        ProcedureVo procedureVo = new ProcedureVo(pFanstarInsertVo);
-        memberDao.callFanstarInsert(procedureVo);
-        String result;
-        if(procedureVo.getRet().equals(Status.팬등록성공.getMessageCode())) {
-            result = gsonUtil.toJson(messageUtil.setJsonOutputVo(new JsonOutputVo(Status.팬등록성공, procedureVo.getData())));
-        } else if(procedureVo.getRet().equals(Status.팬등록_회원아님.getMessageCode())) {
-            result = gsonUtil.toJson(messageUtil.setJsonOutputVo(new JsonOutputVo(Status.팬등록_회원아님)));
-        } else if(procedureVo.getRet().equals(Status.팬등록_스타회원번호이상.getMessageCode())) {
-            result = gsonUtil.toJson(messageUtil.setJsonOutputVo(new JsonOutputVo(Status.팬등록_스타회원번호이상)));
-        } else if(procedureVo.getRet().equals(Status.팬등록_이미팬등록됨.getMessageCode())) {
-            result = gsonUtil.toJson(messageUtil.setJsonOutputVo(new JsonOutputVo(Status.팬등록_이미팬등록됨)));
-        } else{
-            result = gsonUtil.toJson(messageUtil.setJsonOutputVo(new JsonOutputVo(Status.팬등록실패)));
-        }
-        return result;
-    }
-
-    /**
-     * 팬해제
-     */
-    public String callFanstarDelete(P_FanstarDeleteVo pFanstarDeleteVo) {
-        ProcedureVo procedureVo = new ProcedureVo(pFanstarDeleteVo);
-        memberDao.callFanstarDelete(procedureVo);
-        String result;
-
-        if(procedureVo.getRet().equals(Status.팬해제성공.getMessageCode())) {
-            result = gsonUtil.toJson(messageUtil.setJsonOutputVo(new JsonOutputVo(Status.팬해제성공, procedureVo.getData())));
-        } else if(procedureVo.getRet().equals(Status.팬해제_회원아님.getMessageCode())) {
-            result = gsonUtil.toJson(messageUtil.setJsonOutputVo(new JsonOutputVo(Status.팬해제_회원아님)));
-        } else if(procedureVo.getRet().equals(Status.팬해제_스타회원번호이상.getMessageCode())) {
-            result = gsonUtil.toJson(messageUtil.setJsonOutputVo(new JsonOutputVo(Status.팬해제_스타회원번호이상)));
-        } else if(procedureVo.getRet().equals(Status.팬해제_팬아님.getMessageCode())) {
-            result = gsonUtil.toJson(messageUtil.setJsonOutputVo(new JsonOutputVo(Status.팬해제_팬아님)));
-        } else{
-            result = gsonUtil.toJson(messageUtil.setJsonOutputVo(new JsonOutputVo(Status.팬해제실패)));
-        }
-        return result;
-    }
-
-    /**
      * 회원 정보 보기
      */
     public String getMemberInfo(P_InfoVo pInfoVo) throws GlobalException{
