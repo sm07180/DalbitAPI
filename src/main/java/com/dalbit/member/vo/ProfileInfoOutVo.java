35c33935b27679f343e8757de457533f87dc6f6f
고병권 <zox570215@gamil.com>
2020-01-21 16:13:16 +0900

    변수명 정리

---
+++ src/main/java/com/dalbit/member/vo/ProfileInfoOutVo.java
@@ -0,0 +1,48 @@
+package com.dalbit.member.vo;
+
+import com.dalbit.common.vo.ImageVo;
+import com.dalbit.util.DalbitUtil;
+import lombok.Getter;
+import lombok.Setter;
+import lombok.ToString;
+
+@Getter
+@Setter
+@ToString
+public class ProfileInfoOutVo {
+
+    private String nickNm;
+
+    private ImageVo profImg;
+    private String  memNo;
+    private String  gender;
+    private int     age;
+    private String  memId;
+    private ImageVo bgImg;
+    private String  profMsg;
+    private int     level;
+    private int     fanCnt;
+    private int     starCnt;
+    private int     isFan;
+    private int     exp;
+    private int     expNext;
+    private String  grade;
+
+    public ProfileInfoOutVo(P_ProfileInfoVo target) {
+        this.memNo=target.getTarget_mem_no();
+        this.nickNm=target.getNickName();
+        this.gender=target.getMemSex();
+        this.age=target.getAge();
+        this.memId=target.getMemId();
+        this.bgImg=new ImageVo(target.getBackgroundImage(), DalbitUtil.getProperty("server.photo.url"));
+        this.profImg=new ImageVo(target.getProfileImage(), DalbitUtil.getProperty("server.photo.url"));
+        this.profMsg=target.getProfileMsg();
+        this.level=target.getLevel();
+        this.fanCnt=target.getFanCount();
+        this.starCnt=target.getStarCount();
+        this.isFan=target.getEnableFan();
+        this.exp=target.getExp();
+        this.expNext=target.getExpNext();
+        this.grade=target.getGrade();
+    }
+}
