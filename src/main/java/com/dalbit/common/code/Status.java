8e9b9601eb824764ca3b098e9d28372e22bd2bbc
고병권 <zox570215@gamil.com>
2020-01-22 14:24:25 +0900

    오류메시지 추가

--- src/main/java/com/dalbit/common/code/Status.java
+++ src/main/java/com/dalbit/common/code/Status.java
@@ -35,11 +35,13 @@ public enum Status {
     //비밀번호변경
     비밀번호변경실패_회원아님("0", "member.change.password.fail", "비밀번호 변경 실패 시"),
     비밀번호변경성공("1", "member.change.password.success", "비밀번호 변경 성공 시"),
+    비밀번호변경오류("C006", "member.change.password.error", "비밀번호 변경 오류"),
 
     //프로필편집
     프로필편집성공("0", "member.edit.profile.success", "프로필 편집 성공 시"),
     프로필편집실패_회원아님("-1", "member.edit.profile.fail.notUser", "프로필 편집 실패 - 회원이 아닌경우"),
     프로필편집실패_닉네임중복("-2", "member.edit.profile.fail.duplicateNickName", "프로필 편집 실패 - 닉네임이 중복된 경우"),
+    프로필편집오류("C006", "member.edit.profile.error", "프로필 편집 오류"),
 
     //회원팬등록
     팬등록성공("0", "member.insert.fanstar.success", "회원 팬 등록 성공 시"),
@@ -59,43 +61,49 @@ public enum Status {
     회원정보보기_성공("0", "member.info.view.success", "회원 팬 등록 성공 시"),
     회원정보보기_회원아님("-1", "member.no", "회원이 아닐 시"),
     회원정보보기_대상아님("-2", "member.this.no", "회원 팬 등록 실패 시 "),
-    회원정보보기_실패("-C006", "member.info.view.fail", "회원정보보기 실패 시"),
+    회원정보보기_실패("C006", "member.info.view.fail", "회원정보보기 실패 시"),
 
     //회원 방송방 기본설정 조회
     방송방기본설정조회_성공("0", "mypage.broad.basic.suceess", "방송방 기본설정 조회 성공 시"),
     방송방기본설정조회_회원아님("-1", "mypage.broad.basic.no", "회원이 아닐 시"),
+    방송방기본설정조회_오류("C006", "mypage.broad.basic.error", "회원 방송방 기본설정 조회 오류"),
 
     //회원 방송방 기본설정 수정
     방송방기본설정수정_성공("0", "mypage.broad.basic.edit.suceess", "방송방 기본설정 수정 성공 시"),
     방송방기본설정수정_회원아님("-1", "mypage.broad.basic.edit.no", "회원이 아닐 시"),
+    방송방기본설정수정_오류("C006", "mypage.broad.basic.edit.error", "회원 방송방 기본설정 수정 오류"),
 
     //회원 신고하기
     회원신고성공("0", "mypage.member.report.add.suceess", "회원 신고 성공 시"),
     회원신고_요청회원번호_정상아님("-1", "mypage.member.report.add.mem.no", "요청 회원번호 정상 아닐 시"),
     회원신고_신고회원번호_정상아님("-2", "mypage.member.report.add.reported.no", "신고 회원번호 정상 아닐 시"),
     회원신고_이미_신고상태("-3", "mypage.member.reported", "이미 신고 상태 시"),
+    회원신고오류("C006", "mypage.member.reported.error", "회원 신고 신청 중 오류"),
 
     //회원 차단하기
     회원차단성공("0", "mypage.member.block.suceess", "회원 차단 성공 시"),
     회원차단_요청회원번호_정상아님("-1", "mypage.member.block.mem.no", "요청 회원번호 정상 아닐 시"),
-    회원차단_신고회원번호_정상아님("-2", "mypage.member.block.blocked.mem.no", "차단 회원번호 정상 아닐 시"),
-    회원차단_이미_신고상태("-3", "mypage.member.block.blocked", "이미 차단 상태 시"),
+    회원차단_차단단원번호_정상아님("-2", "mypage.member.block.blocked.mem.no", "차단 회원번호 정상 아닐 시"),
+    회원차단_이미_차단상태("-3", "mypage.member.block.blocked", "이미 차단 상태 시"),
+    회원차단오류("C006", "mypage.member.block.error", "회원 차단 신청중 오류"),
 
     //회원 차단해제하기
     회원차단해제성공("0", "mypage.member.block.del.suceess", "회원 차단 해제 성공 시"),
     회원차단해제_요청회원번호_정상아님("-1", "mypage.member.block.del.mem.no", "요청 회원번호 정상 아닐 시"),
     회원차단해제_신고회원번호_정상아님("-2", "mypage.member.block.blocked.del.mem.no", "차단 회원번호 정상 아닐 시"),
     회원차단안된상태("-3", "mypage.member.block.no", "차단 안된 상태 시"),
-
+    회원차단해제오류("C006", "mypage.member.block.error", "차단 해제 신청중 오류"),
 
     //회원 알림설정 조회하기
     알림설정조회성공("0", "mypage.member.notify.sel.suceess", "알림 설정조회 성공 시"),
     알림설정_회원아님("-3", "mypage.member.notify.no", "알림설정 회원아닐 시"),
     알림설정수정성공("0", "mypage.member.notify.edit.suceess", "알림 설정수정 성공 시"),
+    알림설정에러("C006", "mypage.member.notify.error", "알림 설정 에러"),
 
     //회원 방송방 기본설정 수정
     방송방기본설정_수정성공("0", "myprofile.broad.basic.edit.suceess", "방송방 기본설정 수정 성공 시"),
     방송방기본설정_회원아님("-1", "myprofile.broad.basic.edit.no", "회원이 아닐 시"),
+    방송방기본설정_수정에러("C006", "myprofile.broad.basic.edit.error", "방송방 기본설정 수정 에러"),
 
 
     //회원 마이페이지 팬보드 댓글 달기
@@ -106,7 +114,7 @@ public enum Status {
     팬보드_댓글달기실패_depth값_오류("-4", "fanboard.add.fail.wrong.depth", " depth 값 잘못됨"),
 
     //회원 마이페이지 팬보드 댓글 리스트
-    팬보드조회성공("00000", "fanboard.list.comment.success", "팬보드 정보 조회 시"),
+    팬보드조회성공("C001", "fanboard.list.comment.success", "팬보드 정보 조회 시"),
     팬보드_댓글없음("0", "fanboard.list.comment.no", "댓글이 없음"),
     팬보드_요청회원번호_회원아님("-1", "fanboard.list.requestNo.notMember", "요청 회원번호가 회원이 아님"),
     팬보드_스타회원번호_회원아님("-2", "fanboard.list.starNo.notMember","스타 회원번호가 회원이 아님"),
@@ -121,10 +129,11 @@ public enum Status {
     팬보드_댓글삭제실패_삭제권한없음("-6", "fanboard.delete.authorization.not", "삭제 권한이 없음"),
 
     //회원 마이페이지 팬보드 대댓글 보기
-    팬보드_대댓글조회성공("00001", "fanboard.reply.comment.success", "팬보드 대댓글 조회 시"),
+    팬보드_대댓글조회성공("C001", "fanboard.reply.comment.success", "팬보드 대댓글 조회 시"),
     팬보드_대댓글조회실패_대댓글없음("0", "fanboard.reply.comment.no", "대댓글 없음"),
     팬보드_대댓글조회실패_요청회원번호_회원아님("-1", "fanboard.reply.requestNo.notMember", "요청 회원번호가 회원이 아님"),
     팬보드_대댓글조회실패_스타회원번호_회원아님("-2", "fanboard.reply.starNo.notMember", "스타 회원번호가 회원이 아님"),
+    팬보드_대댓글조회오류("C006", "fanboard.reply.starNo.error", "스타 회원번호가 회원 조회오류"),
 
 
 
