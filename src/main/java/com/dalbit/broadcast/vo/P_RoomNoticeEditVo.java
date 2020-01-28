8ed410e67f5ad86b1a5f2ec46f41700b1adb31a0
강다인 <ddainkk@gmail.com>
2020-01-28 13:12:48 +0900

    공지사항 수정 vo

---
+++ src/main/java/com/dalbit/broadcast/vo/P_RoomNoticeEditVo.java
@@ -0,0 +1,15 @@
+package com.dalbit.broadcast.vo;
+
+import lombok.Getter;
+import lombok.Setter;
+
+@Getter
+@Setter
+public class P_RoomNoticeEditVo {
+
+    /* input */
+    private String mem_no;      // 회원번호
+    private String room_no;     // 방송방번호
+    private String notice;      // 공지사항
+
+}
