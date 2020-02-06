319a7b56770176e05fe2eab6e1b5996546c656d8
이재은 <coreano81@inforex.co.kr>
2020-02-06 17:47:13 +0900

    파라메터 로그 출력

--- src/main/java/com/dalbit/interceptor/ParamCheckInterceptor.java
+++ src/main/java/com/dalbit/interceptor/ParamCheckInterceptor.java
@@ -1,31 +1,33 @@
 package com.dalbit.interceptor;
 
-import com.dalbit.common.code.ErrorStatus;
-import com.dalbit.exception.GlobalException;
-import com.dalbit.util.DalbitUtil;
+//import com.dalbit.common.code.ErrorStatus;
+//import com.dalbit.exception.GlobalException;
+//import com.dalbit.util.DalbitUtil;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.web.servlet.handler.HandlerInterceptorAdapter;
 
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 import java.util.Enumeration;
-import java.util.regex.Pattern;
+//import java.util.regex.Pattern;
 
 @Slf4j
 public class ParamCheckInterceptor extends HandlerInterceptorAdapter {
 
-    final String intPattern1 = "\\d+";
+    /*final String intPattern1 = "\\d+";
     final String intPattern2 = "\\-\\d+";
     final String floatPattern1 = "\\d+\\.\\d+";
     final String floatPattern2 = "\\-\\d+\\.\\d+";
     final String datePattern1 = "\\d{8}";
-    final String datePattern2 = "\\d{4}[.-]\\d{2}[.-]\\d{2}";
+    final String datePattern2 = "\\d{4}[.-]\\d{2}[.-]\\d{2}";*/
 
     @Override
     public boolean preHandle(HttpServletRequest request, HttpServletResponse response, Object handler) throws Exception{
+        log.debug("========================== Start Request uri = " + request.getRequestURI());
         for(Enumeration<String> itertor = (Enumeration<String>)request.getParameterNames(); itertor.hasMoreElements();){
             String key = itertor.nextElement();
-            if(key.startsWith("i_")){
+            log.debug("========================================= " + request.getRequestURI() + " " + key + " = |" + request.getParameterValues(key) + "|");
+            /*if(key.startsWith("i_")){
                 String[] values = request.getParameterValues(key);
                 if (values != null && values.length > 0){
                     if(values.length == 1){
@@ -70,12 +72,12 @@ public class ParamCheckInterceptor extends HandlerInterceptorAdapter {
                         }
                     }
                 }
-            }
+            }*/
         }
-
+        log.debug("========================== End Request uri = " + request.getRequestURI());
         return true;
     }
-
+    /*
     public boolean checkInteger(String value) {
         return Pattern.matches(intPattern1, value) || Pattern.matches(intPattern2, value);
     }
@@ -86,5 +88,5 @@ public class ParamCheckInterceptor extends HandlerInterceptorAdapter {
 
     public boolean checkDate(String value) {
         return (Pattern.matches(datePattern1, value) || Pattern.matches(datePattern2, value)) &&  DalbitUtil.isDate(value);
-    }
+    }*/
 }
