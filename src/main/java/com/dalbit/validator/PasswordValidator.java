a8088b8de662e0e18282d08c74e2c722ed0a5e24
이형원 <hwlee@inforex.co.kr>
2020-02-10 14:21:45 +0900

    비밀번호 정규식 유틸 추가

---
+++ src/main/java/com/dalbit/validator/PasswordValidator.java
@@ -0,0 +1,23 @@
+package com.dalbit.validator;
+
+import com.dalbit.util.DalbitUtil;
+import com.dalbit.validator.annotation.Password;
+import com.dalbit.validator.annotation.Sample;
+import lombok.extern.slf4j.Slf4j;
+
+import javax.validation.ConstraintValidator;
+import javax.validation.ConstraintValidatorContext;
+
+@Slf4j
+public class PasswordValidator implements ConstraintValidator<Password, String> {
+    @Override
+    public void initialize(Password password) {
+        log.debug("Password pattern.initialize()...");
+    }
+
+    @Override
+    public boolean isValid(String password, ConstraintValidatorContext constraintValidatorContext) {
+        log.debug(password);
+        return DalbitUtil.isPasswordCheck(password);
+    }
+}
