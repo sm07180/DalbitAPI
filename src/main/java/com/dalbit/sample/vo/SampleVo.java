ac44b1befd6b22c2d4daef7c65e2a006b1695e49
이재호 <leejaeho114@gmail.com>
2020-02-12 11:06:31 +0900

    소스정리

--- src/main/java/com/dalbit/sample/vo/SampleVo.java
+++ src/main/java/com/dalbit/sample/vo/SampleVo.java
@@ -1,14 +1,12 @@
 package com.dalbit.sample.vo;
 
 import com.dalbit.common.vo.BaseVo;
-import com.dalbit.validator.annotation.Sample;
 import lombok.Getter;
 import lombok.Setter;
 
 @Getter @Setter
 public class SampleVo extends BaseVo {
     private String id;
-    @Sample
     private String name;
     private int age;
 
