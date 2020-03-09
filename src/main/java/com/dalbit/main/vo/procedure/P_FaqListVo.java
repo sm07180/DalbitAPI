33cd19e29ccbe4fcc549f3ab16ef5938b634a31d
이형원 <hwlee@inforex.co.kr>
2020-03-09 11:44:30 +0900

    [임시] faq 추가

---
+++ src/main/java/com/dalbit/main/vo/procedure/P_FaqListVo.java
@@ -0,0 +1,36 @@
+package com.dalbit.main.vo.procedure;
+
+import com.dalbit.main.vo.request.FaqListVo;
+import com.dalbit.main.vo.request.NoticeListVo;
+import com.dalbit.util.DalbitUtil;
+import lombok.Getter;
+import lombok.Setter;
+
+import java.util.Date;
+
+@Getter @Setter
+public class P_FaqListVo {
+
+    public P_FaqListVo(){}
+    public P_FaqListVo(FaqListVo faqListVo){
+        int pageNo = DalbitUtil.isEmpty(faqListVo.getPage()) ? 1 : faqListVo.getPage();
+        int pageCnt = DalbitUtil.isEmpty(faqListVo.getRecords()) ? 10 : faqListVo.getRecords();
+
+        setSlct_type(faqListVo.getFaqType());
+        setPageNo(pageNo);
+        setPageCnt(pageCnt);
+    }
+
+    /* Input */
+    private Integer slct_type;
+    private int pageNo;
+    private int pageCnt;
+
+    /* Output */
+    private int faqIdx;
+    private int slctType;
+    private String title;
+    private int topFix;
+    private Date writeDate;
+
+}
