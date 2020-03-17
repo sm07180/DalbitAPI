dcc9b86bf9022237bb39ad43c6c2dae0cf379db1
이형원 <hwlee@inforex.co.kr>
2020-03-18 08:56:02 +0900

    참여시 bj,gst memId 추가

--- src/main/java/com/dalbit/broadcast/service/RoomService.java
+++ src/main/java/com/dalbit/broadcast/service/RoomService.java
@@ -185,10 +185,12 @@ public class RoomService {
             returnMap.put("link", target.getLink());
             returnMap.put("state", target.getState());
             returnMap.put("bjMemNo", target.getBjMemNo());
+            returnMap.put("bjMemId", target.getBjMemId());
             returnMap.put("bjNickNm", target.getBjNickNm());
             returnMap.put("bjProfImg", target.getBjProfImg());
             returnMap.put("bjHolder", "https://devimage.dalbitcast.com/holder/gold.png");
             returnMap.put("gstMemNo", target.getGstMemNo() == null ? "" : target.getGstMemNo());
+            returnMap.put("gstMemId", target.getGstMemId() == null ? "" : target.getGstMemId());
             returnMap.put("gstNickNm", target.getGstNickNm() == null ? "" : target.getGstNickNm());
             returnMap.put("gstProfImg", target.getGstProfImg());
             returnMap.put("remainTime", remainTime);
