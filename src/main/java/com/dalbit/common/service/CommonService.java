60114e9e325e341446c5d7d8e8e1549cd7a9c258
이재은 <coreano81@inforex.co.kr>
2020-03-20 09:11:14 +0900

    아이템 추가

--- src/main/java/com/dalbit/common/service/CommonService.java
+++ src/main/java/com/dalbit/common/service/CommonService.java
@@ -95,6 +95,8 @@ public class CommonService {
         items.add(new ItemVo(Item.애니_당근케익));
         items.add(new ItemVo(Item.애니_선물상자));
         items.add(new ItemVo(Item.애니_런닝머신));
+        items.add(new ItemVo(Item.애니_도넛));
+        items.add(new ItemVo(Item.애니_곰돌이));
         Collections.sort(items, new Comparator<ItemVo>() {
             @Override
             public int compare(ItemVo a, ItemVo b) {
