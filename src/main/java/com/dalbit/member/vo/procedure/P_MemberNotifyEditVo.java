8f663b35fb8b63759f4888217f0bfe1f1dda5c8a
이형원 <hwlee@inforex.co.kr>
2020-04-17 14:26:20 +0900

    라디오 -> 라이브 수정

--- src/main/java/com/dalbit/member/vo/procedure/P_MemberNotifyEditVo.java
+++ src/main/java/com/dalbit/member/vo/procedure/P_MemberNotifyEditVo.java
@@ -13,7 +13,7 @@ public class P_MemberNotifyEditVo {
     private int set_2;          //선물 받은 달 알림
     private int set_3;          //팬 알림
     private int set_4;          //댓글 알림
-    private int set_5;          //달빛 라디오 알림
+    private int set_5;          //달빛 라이브 알림
     private int set_6;          //push 알림
     private int set_7;          //좋아요 알림
 
