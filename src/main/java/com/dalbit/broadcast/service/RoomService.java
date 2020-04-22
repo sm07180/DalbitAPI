07fde84b446b23039f858091cc110d10a6ed4ff4
이재은 <coreano81@inforex.co.kr>
2020-04-22 14:36:33 +0900

    /broad/state 에서 방상태 변경 제외

--- src/main/java/com/dalbit/broadcast/service/RoomService.java
+++ src/main/java/com/dalbit/broadcast/service/RoomService.java
@@ -845,9 +845,9 @@ public class RoomService {
             }
             pRoomStateUpdateVo.setState(state);
 
-            if(old_state == state){
+            /*if(old_state == state){
                 return gsonUtil.toJson(new JsonOutputVo(Status.방송방상태변경_성공));
-            }
+            }*/
 
             ProcedureVo procedureVo = new ProcedureVo(pRoomStateUpdateVo);
             roomDao.callBroadCastRoomStateUpate(procedureVo);
