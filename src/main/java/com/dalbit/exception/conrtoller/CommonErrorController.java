908097b088b6d0e91df0ad36a775ce01b4780de6
이재은 <coreano81@inforex.co.kr>
2020-04-29 10:04:04 +0900

    Exception 처리 변경

--- src/main/java/com/dalbit/exception/conrtoller/CommonErrorController.java
+++ src/main/java/com/dalbit/exception/conrtoller/CommonErrorController.java
@@ -2,6 +2,7 @@ package com.dalbit.exception.conrtoller;
 
 import com.dalbit.common.code.Status;
 import com.dalbit.common.service.CommonService;
+import com.dalbit.common.vo.DeviceVo;
 import com.dalbit.common.vo.JsonOutputVo;
 import com.dalbit.common.vo.procedure.P_ErrorLogVo;
 import com.dalbit.exception.GlobalException;
@@ -16,6 +17,8 @@ import org.springframework.web.bind.annotation.RestController;
 
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
+import java.io.PrintWriter;
+import java.io.StringWriter;
 
 @Slf4j
 @ControllerAdvice
@@ -32,13 +35,30 @@ public class CommonErrorController {
     public String exceptionHandle(GlobalException globalException, HttpServletResponse response, HttpServletRequest request){
         DalbitUtil.setHeader(request, response);
 
-        P_ErrorLogVo apiData = new P_ErrorLogVo();
-        apiData.setOs("API");
-        apiData.setMem_no(MemberVo.getMyMemNo(request));
-        apiData.setDtype(globalException.getMethodName());
-        apiData.setCtype(request.getRequestURL().toString());
-        apiData.setDesc(DalbitUtil.isEmpty(globalException.getData()) ? globalException.getValidationMessageDetail().toString() : globalException.getData().toString());
-        commonService.saveErrorLog(apiData);
+        try {
+            DeviceVo deviceVo = new DeviceVo(request);
+            P_ErrorLogVo apiData = new P_ErrorLogVo();
+            apiData.setOs("API");
+            apiData.setDtype(deviceVo.getOs()+"|"+globalException.getMethodName());
+            apiData.setVersion(deviceVo.getAppVersion());
+            apiData.setBuild(deviceVo.getAppBuild());
+            apiData.setMem_no(MemberVo.getMyMemNo(request));
+            apiData.setCtype(request.getRequestURL().toString());
+            String desc = "";
+            if(!DalbitUtil.isEmpty(globalException.getData())){
+                desc = "Data : \n" + globalException.getData().toString() + "\n";
+            }
+            if(!DalbitUtil.isEmpty(globalException.getValidationMessageDetail())){
+                desc += "Validation : \n" + globalException.getValidationMessageDetail().toString() + "\n";
+            }
+            StringWriter sw = new StringWriter();
+            globalException.printStackTrace(new PrintWriter(sw));
+            if(sw != null){
+                desc += "GlobalException : \n" + sw.toString();
+            }
+            apiData.setDesc(desc);
+            commonService.saveErrorLog(apiData);
+        }catch (Exception e){}
 
         if(globalException.getErrorStatus() != null){
             return gsonUtil.toJson(new JsonOutputVo(globalException.getErrorStatus(), globalException.getData(), globalException.getValidationMessageDetail(), globalException.getMethodName()));
@@ -50,6 +70,27 @@ public class CommonErrorController {
     @ExceptionHandler(Exception.class)
     public String exceptionHandle(Exception exception, HttpServletResponse response, HttpServletRequest request){
         DalbitUtil.setHeader(request, response);
+
+        try {
+            DeviceVo deviceVo = new DeviceVo(request);
+            P_ErrorLogVo apiData = new P_ErrorLogVo();
+            apiData.setOs("API");
+            apiData.setDtype(deviceVo.getOs()+"|"+request.getMethod());
+            apiData.setVersion(deviceVo.getAppVersion());
+            apiData.setBuild(deviceVo.getAppBuild());
+            apiData.setCtype(request.getRequestURL().toString());
+            String desc = "";
+            if(!DalbitUtil.isEmpty(request.getParameterMap())){
+                desc = "Data : \n" + gsonUtil.toJson(request.getParameterMap()) + "\n";
+            }
+            StringWriter sw = new StringWriter();
+            exception.printStackTrace(new PrintWriter(sw));
+            if(sw != null){
+                desc += "Exception : \n" + sw.toString();
+            }
+            apiData.setDesc(desc);
+            commonService.saveErrorLog(apiData);
+        }catch (Exception e){}
 
         return gsonUtil.toJson(new JsonOutputVo(Status.비즈니스로직오류, exception.getMessage(), null, request.getMethod() + ""));
     }
