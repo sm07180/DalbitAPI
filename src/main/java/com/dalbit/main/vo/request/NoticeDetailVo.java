6cc1c14637899fdc83945b6c16dd5b9af1c826bf
이재은 <coreano81@inforex.co.kr>
2020-05-10 16:36:52 +0900

    오류수정

--- src/main/java/com/dalbit/main/vo/request/NoticeDetailVo.java
+++ src/main/java/com/dalbit/main/vo/request/NoticeDetailVo.java
@@ -3,11 +3,12 @@ package com.dalbit.main.vo.request;
 import lombok.Getter;
 import lombok.Setter;
 
+import javax.validation.constraints.Min;
 import javax.validation.constraints.NotNull;
 
 @Getter @Setter
 public class NoticeDetailVo {
 
-    @NotNull
+    @NotNull @Min(0)
     private int noticeIdx;
 }
