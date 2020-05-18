d718948031a0f48f884ea19e052789512819060d
이재호 <leejaeho114@gmail.com>
2020-05-18 11:25:20 +0900

    validation에러, 비즈니스 에러 시 로그 출력 추가.

--- src/main/java/com/dalbit/exception/GlobalException.java
+++ src/main/java/com/dalbit/exception/GlobalException.java
@@ -6,10 +6,12 @@ import com.dalbit.common.vo.JsonOutputVo;
 import lombok.Getter;
 import lombok.Setter;
 import lombok.ToString;
+import lombok.extern.slf4j.Slf4j;
 
 import java.util.ArrayList;
 import java.util.HashMap;
 
+@Slf4j
 @ToString
 @Setter
 @Getter
@@ -29,17 +31,24 @@ public class GlobalException extends Exception {
     private ArrayList validationMessageDetail;
 
     public GlobalException(ErrorStatus errorStatus, String methodName){
+
+        //printErrorLog(status, data, validationMessageDetail, methodName, isCustomMessage);
+
         setErrorStatus(errorStatus);
         setMethodName(methodName);
     }
 
     public GlobalException(ErrorStatus errorStatus, Object data, String methodName){
+
+
+
         setErrorStatus(errorStatus);
         setData(data);
         setMethodName(methodName);
     }
 
     public GlobalException(ErrorStatus errorStatus, Object data, ArrayList validationMessageDetail, String methodName){
+        log.error(super.getMessage());
         setErrorStatus(errorStatus);
         setData(data);
         setValidationMessageDetail(validationMessageDetail);
@@ -48,17 +57,26 @@ public class GlobalException extends Exception {
 
 
     public GlobalException(Status status, String methodName){
+
+        printErrorLog(status, null, null, methodName, false);
+
         setStatus(status);
         setMethodName(methodName);
     }
 
     public GlobalException(Status status, Object data, String methodName){
+
+        printErrorLog(status, data, null, methodName, false);
+
         setStatus(status);
         setData(data);
         setMethodName(methodName);
     }
 
     public GlobalException(Status status, Object data, ArrayList validationMessageDetail, String methodName){
+
+        printErrorLog(status, data, validationMessageDetail, methodName, false);
+
         setStatus(status);
         setData(data);
         setValidationMessageDetail(validationMessageDetail);
@@ -66,6 +84,9 @@ public class GlobalException extends Exception {
     }
 
     public GlobalException(Status status, Object data, ArrayList validationMessageDetail, String methodName, boolean isCustomMessage){
+
+        printErrorLog(status, data, validationMessageDetail, methodName, isCustomMessage);
+
         setStatus(status);
         setData(data);
         setValidationMessageDetail(validationMessageDetail);
@@ -83,6 +104,34 @@ public class GlobalException extends Exception {
 
     public static JsonOutputVo throwException(Status status, Object data, String methodName){
         return throwException(status, data, methodName);
+    }
+
+    public void printErrorLog(Status status, Object data, ArrayList validationMessageDetail, String methodName, boolean isCustomMessage){
+        if(status.getMessageCode().equals(Status.벨리데이션체크.getMessageCode())){
+            log.error("messageCode : {}", status.getMessageCode());
+            log.error("messageKey : {}", status.getMessageKey());
+            log.error("desc : {}", status.getDesc());
+            log.error("methodName : {}", methodName);
+            validationMessageDetail.stream().forEach(list ->{
+               log.error("validation message : {}", list.toString());
+            });
+
+        }else if(status.getMessageCode().equals(Status.비즈니스로직오류.getMessageCode())){
+            log.error("messageCode : {}", status.getMessageCode());
+            log.error("messageKey : {}", status.getMessageKey());
+            log.error("desc : {}", status.getDesc());
+            log.error("methodName : {}", methodName);
+            log.error("{}", super.getStackTrace());
+
+        }else if(status.getMessageCode().equals(Status.부적절한문자열.getMessageCode())){
+            log.error("messageCode : {}", status.getMessageCode());
+            log.error("messageKey : {}", status.getMessageKey());
+            log.error("desc : {}", status.getDesc());
+            log.error("methodName : {}", methodName);
+            log.error("{}", super.getStackTrace());
+
+        }
+
     }
 
 }
