93f47d240f735bb9c2c92d59c4f6d44faefb8bb8
이재은 <coreano81@inforex.co.kr>
2020-05-25 13:31:28 +0900

    프레임 분기 제거

--- src/main/java/com/dalbit/member/vo/ProfileInfoOutVo.java
+++ src/main/java/com/dalbit/member/vo/ProfileInfoOutVo.java
@@ -61,7 +61,7 @@ public class ProfileInfoOutVo extends BaseVo {
         this.starCnt = target.getStarCount();
         this.isFan = target.getEnableFan();
         this.roomNo = target.getRoom_no();
-        this.holder = "https://image.dalbitlive.com/level/frame/200513/AAA/ico_frame_" + this.level + ".png";
+        this.holder = "https://image.dalbitlive.com/level/frame/200525/AAA/ico_frame_" + this.level + ".png";
         if(target_mem_no.equals(mem_no)){
             this.exp = target.getExp();
             this.expBegin = target.getExpBegin();
