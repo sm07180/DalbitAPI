249fa74b34f99d5864612d054223349c831695aa
이형원 <hwlee@inforex.co.kr>
2020-06-03 18:43:36 +0900

    빠른말 50자 제한수정

--- src/main/java/com/dalbit/member/vo/request/ShortCutEditVo.java
+++ src/main/java/com/dalbit/member/vo/request/ShortCutEditVo.java
@@ -14,7 +14,7 @@ public class ShortCutEditVo {
     private Integer orderNo;
     @NotBlank @Size(max = 4)
     private String order;
-    @NotBlank @Size (max = 100)
+    @NotBlank @Size (max = 50)
     private String text;
 
     private String isOn;
