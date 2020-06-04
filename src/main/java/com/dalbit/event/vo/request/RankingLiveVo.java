1d5f40d8b5a1d98ae34124a9eab2226fccd8d437
이재은 <coreano81@inforex.co.kr>
2020-06-04 12:45:09 +0900

    파라메터 밸리데이션

--- src/main/java/com/dalbit/event/vo/request/RankingLiveVo.java
+++ src/main/java/com/dalbit/event/vo/request/RankingLiveVo.java
@@ -3,11 +3,16 @@ package com.dalbit.event.vo.request;
 import lombok.Getter;
 import lombok.Setter;
 
+import javax.validation.constraints.Max;
+import javax.validation.constraints.Min;
+import javax.validation.constraints.NotNull;
+
 @Getter
 @Setter
 public class RankingLiveVo {
 
     private String memNo;                  //회원번호
+    @NotNull @Min(1) @Max(3)
     private Integer slctType;              //랭킹구분(1: 경험치, 2: 좋아요, 3: 선물 )
 
 }
--- src/main/java/com/dalbit/event/vo/request/RankingResultVo.java
+++ src/main/java/com/dalbit/event/vo/request/RankingResultVo.java
@@ -3,11 +3,17 @@ package com.dalbit.event.vo.request;
 import lombok.Getter;
 import lombok.Setter;
 
+import javax.validation.constraints.Max;
+import javax.validation.constraints.Min;
+import javax.validation.constraints.NotNull;
+
 @Getter
 @Setter
 public class RankingResultVo {
 
     private String memNo;                  //회원번호
+    @NotNull @Min(1) @Max(3)
     private Integer slctType;              //랭킹구분(1: 경험치, 2: 좋아요, 3: 선물 )
+    @NotNull @Min(1) @Max(3)
     private Integer round;                  //회차(1, 2, 3 )
 }
