d0b1177c80d53a4568daef9d752cc3bfd6df474e
이재은 <coreano81@inforex.co.kr>
2020-06-29 16:43:34 +0900

    팬뱃지 컬럼 변경 대응

--- src/main/java/com/dalbit/common/vo/FanBadgeVo.java
+++ src/main/java/com/dalbit/common/vo/FanBadgeVo.java
@@ -9,11 +9,20 @@ public class FanBadgeVo {
     private String icon;
     private String startColor;
     private String endColor;
+    private String image;
 
     public FanBadgeVo(String text, String icon, String startColor, String endColor){
         this.text = text;
         this.icon = icon;
         this.startColor = startColor;
         this.endColor = endColor;
+    }
+
+    public FanBadgeVo(String text, String icon, String startColor, String endColor, String image){
+        this.text = text;
+        this.icon = icon;
+        this.startColor = startColor;
+        this.endColor = endColor;
+        this.image = image;
     }
 }
