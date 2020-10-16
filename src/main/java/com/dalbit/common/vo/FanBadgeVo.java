58a824181f9590110c343c5a8cde2c0641076d44
이재호 <leejaeho114@gmail.com>
2020-10-16 17:12:26 +0900

    팬 뱃지 수정.

--- src/main/java/com/dalbit/common/vo/FanBadgeVo.java
+++ src/main/java/com/dalbit/common/vo/FanBadgeVo.java
@@ -11,6 +11,15 @@ public class FanBadgeVo {
     private String endColor;
     private String image;
     private String imageSmall;
+    private String frameTop;
+    private String frameChat;
+    private String frameAni;
+    private String tipMsg;
+    private String explainMsg;
+    private String enterAni;
+    private String enterBgImg;
+    private String msgBorderSrtColor;
+    private String msgBorderEndColor;
 
     public FanBadgeVo(String text, String icon, String startColor, String endColor){
         this.text = text;
@@ -34,5 +43,23 @@ public class FanBadgeVo {
         this.endColor = endColor;
         this.image = image;
         this.imageSmall = imageSmall;
+    }
+
+    public FanBadgeVo(String text, String icon, String startColor, String endColor, String image, String imageSmall, String frameTop, String frameChat, String frameAni, String tipMsg, String explainMsg, String enterAni, String enterBgImg, String msgBorderSrtColor, String msgBorderEndColor){
+        this.text = text;
+        this.icon = icon;
+        this.startColor = startColor;
+        this.endColor = endColor;
+        this.image = image;
+        this.imageSmall = imageSmall;
+        this.frameTop = frameTop;
+        this.frameChat = frameChat;
+        this.frameAni = frameAni;
+        this.tipMsg = tipMsg;
+        this.explainMsg = explainMsg;
+        this.enterAni = enterAni;
+        this.enterBgImg = enterBgImg;
+        this.msgBorderSrtColor = msgBorderSrtColor;
+        this.msgBorderEndColor = msgBorderEndColor;
     }
 }
