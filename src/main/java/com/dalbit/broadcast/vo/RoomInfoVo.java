90ecb0ec03eb5c395ea72a975f9818126837b2cd
이재은 <coreano81@inforex.co.kr>
2020-11-26 10:04:06 +0900

    liveBadgeView 주석

--- src/main/java/com/dalbit/broadcast/vo/RoomInfoVo.java
+++ src/main/java/com/dalbit/broadcast/vo/RoomInfoVo.java
@@ -32,7 +32,7 @@ public class RoomInfoVo {
     private boolean djListenerOut;
     private boolean listenerIn;
     private boolean listenerOut;
-    private boolean liveBadgeView;
+    //private boolean liveBadgeView;
     private boolean isFreeze;
     private boolean isExtend;
     /* DJ정보 */
@@ -109,7 +109,7 @@ public class RoomInfoVo {
         this.djListenerOut = (boolean) settingMap.get("djListenerOut");
         this.listenerIn = (boolean) settingMap.get("listenerIn");
         this.listenerOut = (boolean) settingMap.get("listenerOut");
-        this.liveBadgeView = (boolean) settingMap.get("liveBadgeView");
+        //this.liveBadgeView = (boolean) settingMap.get("liveBadgeView");
 
         //TODO 출석체크이벤트 종료 시 구분 처리 필요 추후...
         if(memberInfoVo.getAuth() == 3){
