83eb05e8bad57668010108380e3975204d3ddb64
이형원 <hwlee@inforex.co.kr>
2020-12-15 10:47:41 +0900

    인싸티콘(이모티콘) 클릭 업데이트 강제 리턴

--- src/main/java/com/dalbit/member/controller/MypageController.java
+++ src/main/java/com/dalbit/member/controller/MypageController.java
@@ -731,13 +731,13 @@ public class MypageController {
             msgSlct = 1;
         } else {
             msgSlct = 2;
+            return gsonUtil.toJson(new JsonOutputVo(Status.메시지클릭업데이트_성공));
         }
 
         apiData.setMsg_slct(msgSlct);
         apiData.setMsg_idx(msgClickUpdateVo.getMsgIdx());
 
         String result = mypageService.msgClickUpdate(apiData);
-
         return result;
     }
 
