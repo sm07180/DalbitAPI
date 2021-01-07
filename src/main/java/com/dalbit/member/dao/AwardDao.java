68b583b8418e145098bbbba256e0f8c5e0ab97ea
이형원 <hwlee@inforex.co.kr>
2021-01-07 10:55:56 +0900

    Member Change Slave

--- src/main/java/com/dalbit/member/dao/AwardDao.java
+++ src/main/java/com/dalbit/member/dao/AwardDao.java
@@ -4,14 +4,13 @@ import com.dalbit.common.vo.ProcedureVo;
 import com.dalbit.member.vo.procedure.P_AwardListVo;
 import com.dalbit.member.vo.procedure.P_AwardVoteResultVo;
 import org.springframework.stereotype.Repository;
+import org.springframework.transaction.annotation.Transactional;
 
 import java.util.List;
 
 @Repository
 public interface AwardDao {
     List<P_AwardListVo> callAwardList(ProcedureVo procedureVo);
-
     ProcedureVo callAwardVote(ProcedureVo procedureVo);
-
     List<P_AwardVoteResultVo> callAwardVoteResult(ProcedureVo procedureVo);
 }
--- src/main/java/com/dalbit/member/dao/ProfileDao.java
+++ src/main/java/com/dalbit/member/dao/ProfileDao.java
@@ -13,12 +13,12 @@ public interface ProfileDao {
     @Transactional(readOnly = true)
     ProcedureVo callMemberInfo(ProcedureVo procedureVo);
     ProcedureVo callMemberFanboardAdd(ProcedureVo procedureVo);
-    //@Transactional(readOnly = true)
+    @Transactional(readOnly = true)
     List<P_FanboardListVo> callMemberFanboardList(ProcedureVo procedureVo);
     ProcedureVo callMemberFanboardDelete(ProcedureVo procedureVo);
-    //@Transactional(readOnly = true)
+    @Transactional(readOnly = true)
     List<P_FanboardReplyVo> callMemberFanboardReply(ProcedureVo procedureVo);
-    //@Transactional(readOnly = true)
+    @Transactional(readOnly = true)
     List<P_FanRankingVo> callMemberFanRanking(ProcedureVo procedureVo);
     //@Transactional(readOnly = true)
     ProcedureVo callMemberLevelUpCheck(ProcedureVo procedureVo);
@@ -27,23 +27,23 @@ public interface ProfileDao {
     @Transactional(readOnly = true)
     List<P_StarRankingVo> selectStarRanking(P_StarRankingVo procedureVo);
 
-    //@Transactional(readOnly = true)
+    @Transactional(readOnly = true)
     List<P_FanListVo> callFanList(ProcedureVo procedureVo);
 
-    //@Transactional(readOnly = true)
+    @Transactional(readOnly = true)
     List<P_FanListNewVo> callFanListNew(ProcedureVo procedureVo);
 
-    //@Transactional(readOnly = true)
+    @Transactional(readOnly = true)
     ProcedureVo callFanMemo(ProcedureVo procedureVo);
 
     ProcedureVo callFanMemoSave(ProcedureVo procedureVo);
 
     ProcedureVo callFanEdit(ProcedureVo procedureVo);
 
-    //@Transactional(readOnly = true)
+    @Transactional(readOnly = true)
     List<P_StarListNewVo> callStarListNew(ProcedureVo procedureVo);
 
-    //@Transactional(readOnly = true)
+    @Transactional(readOnly = true)
     ProcedureVo callStarMemo(ProcedureVo procedureVo);
 
     ProcedureVo callStarMemoSave(ProcedureVo procedureVo);
