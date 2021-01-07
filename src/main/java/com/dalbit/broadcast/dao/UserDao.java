4638eb92943dd45e93e2b4765ba5ca5792fef64d
이형원 <hwlee@inforex.co.kr>
2021-01-07 10:58:36 +0900

    User Change Slave

--- src/main/java/com/dalbit/broadcast/dao/UserDao.java
+++ src/main/java/com/dalbit/broadcast/dao/UserDao.java
@@ -3,6 +3,7 @@ package com.dalbit.broadcast.dao;
 import com.dalbit.broadcast.vo.procedure.P_RoomMemberListVo;
 import com.dalbit.common.vo.ProcedureVo;
 import org.springframework.stereotype.Repository;
+import org.springframework.transaction.annotation.Transactional;
 //import org.springframework.transaction.annotation.Transactional;
 
 import java.util.HashMap;
@@ -10,17 +11,17 @@ import java.util.List;
 
 @Repository
 public interface UserDao {
-    //@Transactional(readOnly = true)
+    @Transactional(readOnly = true)
     List<P_RoomMemberListVo> callBroadCastRoomMemberList(ProcedureVo procedureVo);
     ProcedureVo callBroadCastRoomKickout(ProcedureVo procedureVo);
-    //@Transactional(readOnly = true)
+    @Transactional(readOnly = true)
     ProcedureVo callMemberInfo(ProcedureVo procedureVo);
     ProcedureVo callBroadCastRoomManagerAdd(ProcedureVo procedureVo);
     ProcedureVo callBroadCastRoomManagerDel(ProcedureVo procedureVo);
     ProcedureVo callFanstarInsert(ProcedureVo procedureVo);
     ProcedureVo callFanstarDelete(ProcedureVo procedureVo);
 
-    //@Transactional(readOnly = true)
+    @Transactional(readOnly = true)
     HashMap selectGuestStreamInfo(HashMap params);
     int updateGuestStreamInfo(HashMap params);
 }
