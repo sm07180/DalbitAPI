c47a36308e2aaefe1d9bc6a848629c9926b2e77d
이재은 <coreano81@inforex.co.kr>
2021-04-15 08:47:33 +0900

    클립 주간 테이블 최대날자 추가 2021-04-12 넘으면 최대날짜로 조회

--- src/main/java/com/dalbit/clip/vo/procedure/P_ClipRecommendListInputVo.java
+++ src/main/java/com/dalbit/clip/vo/procedure/P_ClipRecommendListInputVo.java
@@ -3,10 +3,12 @@ package com.dalbit.clip.vo.procedure;
 import com.dalbit.clip.vo.request.ClipRecommendListInputVo;
 import com.dalbit.common.vo.P_ApiVo;
 import com.dalbit.member.vo.MemberVo;
+import com.dalbit.util.DalbitUtil;
 import lombok.Getter;
 import lombok.Setter;
 
 import javax.servlet.http.HttpServletRequest;
+import java.util.Calendar;
 
 @Getter
 @Setter
@@ -20,6 +22,26 @@ public class P_ClipRecommendListInputVo extends P_ApiVo {
         setMemLogin(clipRecommendListInputVo.getIsLogin() == false ? 0 : 1);
         setClickYn(clipRecommendListInputVo.getIsClick() == null || clipRecommendListInputVo.getIsClick() == false ? 0 : 1);
         setRecDate(clipRecommendListInputVo.getRecDate());
+        if(DalbitUtil.isEmpty(this.recDate)){
+            setRecDate("2021-04-12");
+        }else{
+            Calendar reqDate = Calendar.getInstance();
+            Calendar maxDate = Calendar.getInstance();
+            maxDate.set(Calendar.YEAR, 2021);
+            maxDate.set(Calendar.MONTH, Calendar.APRIL);
+            maxDate.set(Calendar.DAY_OF_MONTH, 12);
+            String[] reqD = this.recDate.split("-");
+            try {
+                reqDate.set(Calendar.YEAR, Integer.parseInt(reqD[0]));
+                reqDate.set(Calendar.MONTH, Integer.parseInt(reqD[1]) - 1);
+                reqDate.set(Calendar.DAY_OF_MONTH, Integer.parseInt(reqD[2]));
+                if (reqDate.after(maxDate)) {
+                    setRecDate("2021-04-12");
+                }
+            }catch(Exception e){
+                setRecDate("2021-04-12");
+            }
+        }
     }
 
     private String mem_no;
