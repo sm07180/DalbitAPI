7d5de5283d1a6e07421ae9c53792d007cba0965f
박성민 <sm07180>
2021-10-26 15:45:07 +0900

    #7044 룰렛, 공지 정책 변경

--- src/main/java/com/dalbit/broadcast/vo/request/MiniGameAddVo.java
+++ src/main/java/com/dalbit/broadcast/vo/request/MiniGameAddVo.java
@@ -17,7 +17,7 @@ public class MiniGameAddVo {
     @NotNull(message = "{\"ko_KR\" : \"방번호를\"}")
     private String roomNo;
 
-    @Max(message = "{\"ko_KR\" : \"금액을\"}", value = 100)
+    @Max(message = "{\"ko_KR\" : \"금액을\"}", value = 1000)
     private int payAmt;
 
     private int optCnt = 2;
