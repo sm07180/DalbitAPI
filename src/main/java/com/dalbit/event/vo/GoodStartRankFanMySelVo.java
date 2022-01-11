224348104b5b6eded4dfee64e1534036102d076c
박성민 <sm07180>
2022-01-11 15:54:14 +0900

    #7404 굿스타트 이벤트 수정

--- src/main/java/com/dalbit/event/vo/GoodStartRankFanMySelVo.java
+++ src/main/java/com/dalbit/event/vo/GoodStartRankFanMySelVo.java
@@ -7,8 +7,6 @@ import lombok.Setter;
 @Getter
 @Setter
 public class GoodStartRankFanMySelVo {
-    private String the_date; // 집계일자
-    private String the_seq; // 회차[1:6시,2:12시,3:18시,4:24시]
     private String good_no; // 이벤트 회차 번호
     private String mem_no; // 회원번호
     private String mem_id; // 아이디
@@ -19,8 +17,8 @@ public class GoodStartRankFanMySelVo {
     private String mem_exp_score; // 경험치점수
     private String mem_special_score; // 스페셩점수
     private String tot_mem_score; // 랭킹점수
-    private String rank_no; // 이번회차 순위
-    private String yest_rank_no; // 전회차 순위
+    private String my_rank_no; // 내순위
+
     private ImageVo profImg;
     private String holderBg;
     private String[] levelColor;
--- src/main/java/com/dalbit/event/vo/GoodStartRankNewBjMySelVo.java
+++ src/main/java/com/dalbit/event/vo/GoodStartRankNewBjMySelVo.java
@@ -7,21 +7,19 @@ import lombok.Setter;
 @Getter
 @Setter
 public class GoodStartRankNewBjMySelVo {
-    private String the_date; //  집계일자
-    private String the_seq; //  회차[1:6시,2:12시,3:18시,4:24시]
-    private String good_no; //  이벤트 회차 번호
-    private String mem_no; //  회원번호
-    private String mem_id; //  아이디
-    private String mem_sex; //  성별
-    private String mem_nick; //  대화명
-    private int mem_level; //  회원레벨
-    private String image_profile; //  대표이미지
-    private String mem_dal_score; //  선물점수
-    private String mem_tot_like_score; //  좋아요점수
-    private String mem_booster_score; //  부스터점수
-    private String tot_mem_score; //  랭킹점수
-    private String rank_no; //  이번회차 순위
-    private String yest_rank_no; //  전회차 순위
+    private String good_no; // 이벤트 회차 번호
+    private String mem_no; // 회원번호
+    private String mem_id; // 아이디
+    private String mem_sex; // 성별
+    private String mem_nick; // 대화명
+    private int mem_level; // 회원레벨
+    private String image_profile; // 대표이미지
+    private String mem_dal_score; // 선물점수
+    private String mem_tot_like_score; // 좋아요점수
+    private String mem_booster_score; // 부스터점수
+    private String tot_mem_score; // 랭킹점수
+    private String my_rank_no; // 내순위
+
     private ImageVo profImg;
     private String holderBg;
     private String[] levelColor;
