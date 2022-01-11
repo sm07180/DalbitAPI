dc2ef875b40943f031a81596065a419fabd50a17
박성민 <sm07180>
2022-01-11 14:18:58 +0900

    #7404 굿스타트 이벤트

---
+++ src/main/java/com/dalbit/event/vo/GoodStartRankFanMySelVo.java
@@ -0,0 +1,27 @@
+package com.dalbit.event.vo;
+
+import com.dalbit.common.vo.ImageVo;
+import lombok.Getter;
+import lombok.Setter;
+
+@Getter
+@Setter
+public class GoodStartRankFanMySelVo {
+    private String the_date; // 집계일자
+    private String the_seq; // 회차[1:6시,2:12시,3:18시,4:24시]
+    private String good_no; // 이벤트 회차 번호
+    private String mem_no; // 회원번호
+    private String mem_id; // 아이디
+    private String mem_sex; // 성별
+    private String mem_nick; // 대화명
+    private int mem_level; // 회원레벨
+    private String image_profile; // 대표이미지
+    private String mem_exp_score; // 경험치점수
+    private String mem_special_score; // 스페셩점수
+    private String tot_mem_score; // 랭킹점수
+    private String rank_no; // 이번회차 순위
+    private String yest_rank_no; // 전회차 순위
+    private ImageVo profImg;
+    private String holderBg;
+    private String[] levelColor;
+}
