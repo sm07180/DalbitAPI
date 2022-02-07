31d3bced2b605ee5549534f16931e464b2103958
박용훈 <ot9400@naver.com>
2022-02-07 14:47:58 +0900

    피드 조회 수정

--- src/main/java/com/dalbit/member/service/MypageService.java
+++ src/main/java/com/dalbit/member/service/MypageService.java
@@ -891,8 +891,33 @@ public class MypageService {
     }
 
     /**
-     * 마이페이지 피드 조회
-     * memNo : profile 주인 memNo
+     * 피드 리스트 조회
+     *
+     * @Param
+     * memNo 		BIGINT		-- 회원번호 (프로필 주인)
+     * viewMemNo 		BIGINT		-- 회원번호 (프로필 보고있는 유저)
+     * ,pageNo 		INT 		-- 페이지 번호
+     * ,pageCnt 	INT		-- 페이지 당 노출 건수 (Limit)
+     *
+     * @Return
+     * Multi Rows
+     * #1
+     * cnt		INT		--총 수
+     * #2
+     * noticeIdx		BIGINT		-- 번호
+     * mem_no		BIGINT		-- 회원번호
+     * nickName	VARCHAR	--닉네임
+     * memSex		VARCHAR	-- 성별
+     * image_profile	VARCHAR	-- 프로필
+     * title		VARCHAR	-- 제목
+     * contents		VARCHAR	-- 내용
+     * imagePath	VARCHAR	-- 대표사진
+     * topFix		BIGINT		-- 고정여부[0:미고정 ,1:고정]
+     * writeDate		DATETIME	-- 수정일자
+     * readCnt		BIGINT		-- 읽은수
+     * replyCnt		BIGINT		-- 댓글수
+     * rcv_like_cnt	BIGINT		-- 좋아요수
+     * rcv_like_cancel_cnt BIGINT		-- 취소 좋아요수
      */
     public String noticeSelect(String memNo, Integer pageNo, Integer pagePerCnt, HttpServletRequest request) throws GlobalException{
         HashMap paramMap = new HashMap();
