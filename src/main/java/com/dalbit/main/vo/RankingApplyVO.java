051b6457612c71df8a5056986a834bce85cab60a
강알찬 <aaasss86@naver.com>
2022-04-13 13:32:15 +0900

    #7879Fan랭킹 참여 버튼 추가

---
+++ src/main/java/com/dalbit/main/vo/RankingApplyVO.java
@@ -0,0 +1,10 @@
+package com.dalbit.main.vo;
+
+import lombok.Data;
+
+@Data
+public class RankingApplyVO {
+    private String mem_no;
+    private Integer apply_ranking;
+    private String las_upd_date;
+}
