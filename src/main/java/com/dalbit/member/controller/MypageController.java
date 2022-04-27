1dda32712b2edc64e5444ad9f6ff208b2e6348e6
김효빈 <gyqls9973@naver.com>
2022-04-27 16:05:25 +0900

    방송공지, 피드 파라미터 로그 수정

--- src/main/java/com/dalbit/member/controller/MypageController.java
+++ src/main/java/com/dalbit/member/controller/MypageController.java
@@ -1109,7 +1109,7 @@ public class MypageController {
         try {
             return mypageService.feedSelect(feedListVo, request);
         } catch (Exception e) {
-            log.error("MypageController.java / feedSelect() => params: {}, error: {}, memNo: {}", gsonUtil.toJson(feedListVo), e, MemberVo.getMyMemNo(request));
+            log.error("MypageController.java / feedSelect() => params: {}, error: {}, memNo: {}", gsonUtil.toJson(feedListVo), e, gsonUtil.toJson(MemberVo.getMyMemNo(request)));
             HashMap resultMap = new HashMap();
             resultMap.put("list", new ArrayList());
             resultMap.put("paging", new PagingVo(0, 0, 0));
@@ -1237,7 +1237,7 @@ public class MypageController {
         try {
             return mypageService.feedReplyList(param, request);
         } catch (Exception e) {
-            log.error("MypageController.java / feedReplyList Exception / pararm: {}, error: {}, memNo: {}", gsonUtil.toJson(param) ,e, MemberVo.getMyMemNo(request));
+            log.error("MypageController.java / feedReplyList Exception / pararm: {}, error: {}, memNo: {}", gsonUtil.toJson(param) ,e, gsonUtil.toJson(MemberVo.getMyMemNo(request)));
             return gsonUtil.toJson(new JsonOutputVo(Status.공지댓글보기_실패));
         }
     }
@@ -1299,7 +1299,7 @@ public class MypageController {
         try {
             return mypageService.noticeSelect(noticeSelVo.getMemNo(), noticeSelVo.getPageNo(), noticeSelVo.getPageCnt(), request);
         } catch (Exception e) {
-            log.error("MypageController.java / noticeSelect() => params: {}, error: {}, memNo: {}", gsonUtil.toJson(noticeSelVo), e, MemberVo.getMyMemNo(request));
+            log.error("MypageController.java / noticeSelect() => params: {}, error: {}, memNo: {}", gsonUtil.toJson(noticeSelVo), e, gsonUtil.toJson(MemberVo.getMyMemNo(request)));
             HashMap resultMap = new HashMap();
 //            resultMap.put("fixList", new ArrayList());
             resultMap.put("list", new ArrayList());
