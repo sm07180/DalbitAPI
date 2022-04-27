9004b38993b59123cebfc2be304662e47153ab37
이승재 <nonp2@inforex.com>
2022-04-27 14:13:37 +0900

    팀 제도/ 수정사항 커밋

--- src/main/java/com/dalbit/team/vo/TeamParamVo.java
+++ src/main/java/com/dalbit/team/vo/TeamParamVo.java
@@ -4,6 +4,8 @@ import com.fasterxml.jackson.annotation.JsonProperty;
 import lombok.Getter;
 import lombok.Setter;
 
+import java.util.List;
+
 @Getter
 @Setter
 public class TeamParamVo {
@@ -17,16 +19,18 @@ public class TeamParamVo {
     Integer teamNo;             //BIGINT		-- 팀번호
     String reqMemYn;            //CHAR(1)		-- 가입신청 허용여부
     Long masterMemNo;        //BIGINT		-- 팀장 회원번호
+    String masterName;       // 팀장 이름
     String chrgrName;           //VARCHAR(40)	-- 삭제 관리자명 (관리자 삭제시)
     String reqSlct;             //CHAR(1)		-- 신청구분 [r:가입신청, i:초대]
     String delSclt;             //CHAR(1)		-- 탈퇴구분 [ m:팀장탈퇴, t:본인탈퇴, c:관리자탈퇴, e:회원탈퇴 ]
-    Long tmMemNo;            //BIGINT		-- 팀원 회원번호
-    String bgCode;              //CHAR(4)		-- 배지코드
-    Integer pageNo=1;           //INT UNSIGNED		-- 페이지 번호
-    Integer pagePerCnt=100;     //INT UNSIGNED	-- 페이지 당 노출 건수 (Limit)
-    String symbolSlct;          //CHAR(1)		-- 심볼구분 [b:배경, e:테두리, m:메달]
-    String ordSlct;             //CHAR(1)		-- 정렬구분 [f:선호도, c:코드순, i:갱신일순]
-    String listSlct;            //CHAR(1)		-- 리스트구분[s:스타, f:팬]
+    Long tmMemNo;                //BIGINT		-- 팀원 회원번호
+    String bgCode;               //CHAR(4)		-- 배지코드
+    Integer pageNo=1;            //INT UNSIGNED		-- 페이지 번호
+    Integer pagePerCnt=100;      //INT UNSIGNED	-- 페이지 당 노출 건수 (Limit)
+    String symbolSlct;           //CHAR(1)		-- 심볼구분 [b:배경, e:테두리, m:메달]
+    String ordSlct;              //CHAR(1)		-- 정렬구분 [f:선호도, c:코드순, i:갱신일순]
+    String listSlct;             //CHAR(1)		-- 리스트구분[s:스타, f:팬]
+    List<Object> memNoList; // 푸시용 팀 인원 번호
 
     @JsonProperty("tDate")
     private String tDate;
