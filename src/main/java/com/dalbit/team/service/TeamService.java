805697ed9e51302f7d90e3b5336977c50960d174
이정혁 <habetree@gmail.com>
2022-05-04 08:53:49 +0900

    #7791 unused delete

--- src/main/java/com/dalbit/team/service/TeamService.java
+++ src/main/java/com/dalbit/team/service/TeamService.java
@@ -10,7 +10,6 @@ import com.dalbit.common.vo.ResVO;
 import com.dalbit.team.proc.TeamProc;
 import com.dalbit.team.vo.*;
 import com.dalbit.util.DBUtil;
-import com.dalbit.util.GsonUtil;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
