4f7b09231e94635abedbf22381038b83b229dbf7
강알찬 <aaasss86@naver.com>
2022-05-12 13:55:15 +0900

    메인 탑베너

--- src/main/java/com/dalbit/main/service/MainServiceV2.java
+++ src/main/java/com/dalbit/main/service/MainServiceV2.java
@@ -489,11 +489,11 @@ public class MainServiceV2 {
             swiperList.addAll(mainPage.getTopLiveList(bannerMap));
         }
 
-        if (cnt > 10){
-            swiperList = swiperList.subList(0, 10);
-        }
-
         ArrayList<MainSwiperVO> swiperList2 = new ArrayList<>(swiperList.stream().filter(distinctByKey(o-> o.getMem_no())).collect(Collectors.toList()));
+
+        if (cnt > 10){
+            swiperList2 = (ArrayList<MainSwiperVO>) swiperList2.subList(0, 10);
+        }
 
         for(MainSwiperVO vo : swiperList2) {
             vo.setImageProfile(new ImageVo(vo.getImage_profile(), DalbitUtil.getProperty("server.photo.url")));
